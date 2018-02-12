29fb8b04f5f99331590cf58921b1bd9693ddf881
stephen <wqstu1@gmail.com>
2018-02-13 01:09:43 +0800

    Change the way of importing css.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 const FormItem = Form.Item;
-import 'antd/dist/antd.css';
 
 class NormalLoginForm extends React.Component {
   handleSubmit = (e) => {
